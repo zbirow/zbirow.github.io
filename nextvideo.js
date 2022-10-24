ab94ed2af9e36c7bf0eb3226fa617bd725f10fe2
zbirow <77003019+zbirow@users.noreply.github.com>
2022-10-24 21:37:50 +0200

    Update nextvideo.js

--- nextvideo.js
+++ nextvideo.js
@@ -1,4 +1,4 @@
-function nes() {
+function nas() {
   var clicks = document.getElementsByClassName('odc')[0].textContent;
         clicks += 1;
         document.getElementsByClassName("odc").innerHTML = clicks;
