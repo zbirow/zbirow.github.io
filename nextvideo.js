e2bcadd30f001424c040994284ffcfaa1880baa5
zbirow <77003019+zbirow@users.noreply.github.com>
2022-10-25 17:11:56 +0200

    Update nextvideo.js

--- nextvideo.js
+++ nextvideo.js
@@ -2,8 +2,8 @@ function nas() {
  var odc = document.getElementsByClassName('odc')[0].textContent;
         odc += 1;
         document.getElementsByClassName('odc').innerHTML = odc;
-  let two = document.getElementsByClassName("2")[0].src;
-  let three = document.getElementsByClassName("3")[0].src;
+  let two = document.getElementsByClassName("odc2")[0].src;
+  let three = document.getElementsByClassName("odc3")[0].src;
   document.getElementsByClassName("pop")[0].style.display="contents";
   document.getElementsByClassName("iframe")[0].src = two;
     };
