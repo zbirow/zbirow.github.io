c71878dc89ee0ea13d67a366e9ed1af4cb22066f
zbirow <77003019+zbirow@users.noreply.github.com>
2022-10-25 16:44:27 +0200

    Update nextvideo.js

--- nextvideo.js
+++ nextvideo.js
@@ -5,5 +5,5 @@ function nas() {
   let two = document.getElementsByClassName("2")[0].src;
   let three = document.getElementsByClassName("3")[0].src;
   document.getElementsByClassName("pop")[0].style.display="contents";
-  document.getElementsByClassName("iframe")[0].src = two ;}; ';
+  document.getElementsByClassName("iframe")[0].src = two;
     };
