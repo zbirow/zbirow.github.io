26e558a89e3ad4f92fbcb0e8b1ccd74562bec713
zbirow <77003019+zbirow@users.noreply.github.com>
2023-03-03 20:33:24 +0100

    Update downloadQueue.js

--- downloadQueue.js
+++ downloadQueue.js
@@ -6,25 +6,11 @@ class DownloadQueue {
         this.urls = urls;
     }
 }
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-    if (request.message == "folder-download-next") {
-        let url = request.url;
-        if (url) {
+   
+       
             // Change download text
             let text = document.querySelector("brdPlayer brndPlayerPd");
             text.innerHTML =
-                "<br> Pobieram [" +
-                    request.current_download +
-                    "/" +
-                    request.max_length +
-                    "]";
+                "<br> Pobieram </br>";
             // Handle Download
-            await Video.bestFromUrl(url).then((video) => {
-                chrome.runtime.sendMessage({
-                    message: "cda-download-video",
-                    video: video,
-                });
-            });
-        }
-    }
-});
+       
