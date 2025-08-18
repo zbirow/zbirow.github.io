ac710fa40208c771a4623c0d3d6320041c1f8df3
zbirow <77003019+zbirow@users.noreply.github.com>
2025-08-18 16:30:44 +0200

    Delete nextvideo.js

--- nextvideo.js
+++
@@ -1,9 +0,0 @@
-function nas() {
- var odc = document.getElementsByClassName('odc')[0].textContent;
-        odc += 1;
-        document.getElementsByClassName('odc').innerHTML = odc;
-  let two = document.getElementsByClassName("odc2")[0].src;
-  let three = document.getElementsByClassName("odc3")[0].src;
-  document.getElementsByClassName("pop")[0].style.display="contents";
-  document.getElementsByClassName("iframe")[0].src = two;
-    };
